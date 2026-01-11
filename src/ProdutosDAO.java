59e64e8ceefdb7a4975a4f340e50d1e018b86bb4
Davi Santos <jjdeiv14@gmail.com>
2026-01-11 12:33:00 -0300

    Definiçao da tela de cadastro como tela inicial do sistema

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,31 +12,67 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class ProdutosDAO {
     
-    Connection conn;
+  Connection conn;
     PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    ResultSet rs;
+    ArrayList<ProdutosDTO> lista = new ArrayList<>();
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+        conn = new conectaDAO().connectDB();
+
+        try {
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+
+            prep.execute();
+            prep.close();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro no  ao cadastrar: " + e.getMessage());
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
+       String sql = "SELECT * FROM produtos";
+
+        conn = new conectaDAO().connectDB();
+        lista.clear(); // ?evita duplicar dados
+
+        try {
+            prep = conn.prepareStatement(sql);
+            rs = prep.executeQuery();
+
+            while (rs.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getInt("valor"));
+                produto.setStatus(rs.getString("status"));
+
+                lista.add(produto);
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro no ProdutosDAO ao listar: " + e.getMessage());
+        }
+
+        return lista;
+    }
+    
     }
     
     
     
         
-}
+
 
